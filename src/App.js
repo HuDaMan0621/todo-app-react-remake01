9d20493dc26c1ec5952ce7c30e8aeda70dba0383
hudaman0621 <mrhu828@gmail.com>
2020-06-12 15:52:23 -0400

    part1, remove is working

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import Todos from './Todos'
 
 class App extends Component {
   state = {
-    todo: [
+    todos: [
       { id: 1, content: 'buy some milk' },
       { id: 2, content: 'play mario kart' }
     ]
   }
+deleteTodo = (id) => {
+const todos = this.state.todos.filter(todo => { //if this todo is not = to id on 11, we will return true, if it's false, it will filter out and remove 
+  return todo.id !== id  //return true or false here, true to remove, false to keep
+}) 
+this.setState({
+  todos: todos
+})
+}
+
 
   render() {
     return (
-      <div className="App">
+      <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     )
   };
