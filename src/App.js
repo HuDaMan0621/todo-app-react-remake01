e7694caa6a96b1007219d7b7ef63d7490873c7ea
hudaman0621 <mrhu828@gmail.com>
2020-06-14 21:47:19 -0400

    can add, delete and celebrate when nothing is on the todo list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos'
+import AddTodo from './AddTodo'
 
 class App extends Component {
   state = {
@@ -8,24 +9,31 @@ class App extends Component {
       { id: 2, content: 'play mario kart' }
     ]
   }
-deleteTodo = (id) => {
-const todos = this.state.todos.filter(todo => { //if this todo is not = to id on 11, we will return true, if it's false, it will filter out and remove 
-  return todo.id !== id  //return true or false here, true to remove, false to keep
-}) 
-this.setState({
-  todos: todos
-})
-}
-
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => { //if this todo is not = to id on 11, we will return true, if it's false, it will filter out and remove 
+      return todo.id !== id  //return true or false here, true to remove, false to keep
+    })
+    this.setState({
+      todos
+    })
+  }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos
+    })
+  }
 
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     )
   };
 }
 
-  export default App;
+export default App;
